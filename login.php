d93101daa9e3d0622b61757a40a0c225aebbe799
dilshan <chamindu.developer@gmail.com>
2019-10-17 00:12:10 +0530

    dd

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
-$username = null;
-$password = null;
+$username = $_GET['username'];
+$password = $_GET['password'];
 
 $path = $_SERVER['REQUEST_URI'];
 
 $params = explode('/',$path);
 
-$username = $params[count($params)-2];
-$password = $params[count($params)-1];
+//$username = $params[count($params)-2];
+//$password = $params[count($params)-1];
 
 if($username=='username' && $password=='password'){
     echo json_encode(array('message'=>true));
