c06ceb8c76c9eef359f68381f4f1087159e2530f
dilshan <chamindu.developer@gmail.com>
2019-10-17 00:05:52 +0530

    login php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$username = $_GET['username'];
+$password = $_GET['username'];
+
+$path = $_SERVER['REQUEST_URI'];
+
+$params = explode('/',$path);
+
+//$username = $params[count($params)-2];
+//$password = $params[count($params)-1];
+
+if($username=='username' && $password=='password'){
+    echo json_encode(array('message'=>true));
+}else{
+    echo json_encode(array('message'=>false));
+}
+?>
